2146f7b0a0f9dd92f7f4b1fe3344b1ac3efb077a
AndreyChekunov <79117325352@ya.ru>
2022-07-02 20:32:04 +0300

    Task 54 v2

--- Task_54_HW/Program.cs
+++ Task_54_HW/Program.cs
@@ -6,11 +6,7 @@ void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            
-            Console.Write($"{array[i, j]} ");
-        }
+        for (int j = 0; j < array.GetLength(1); j++) Console.Write($"{array[i, j]} ");
         Console.WriteLine();
     }
 }
