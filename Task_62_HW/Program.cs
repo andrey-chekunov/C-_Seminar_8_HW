f60b9d2351edb2b744ceb3287293b9c5997cf3d4
AndreyChekunov <79117325352@ya.ru>
2022-07-06 20:40:28 +0300

    Resolve 62

---
+++ Task_62_HW/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 62: Заполните спирально массив 4 на 4.
+
+Console.WriteLine("Input array size: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[size, size];
+int value = 1;
+int i = 0;
+int j = 0;
+
+while (value <= size * size)
+{
+    array[i, j] = value;
+    if (i <= j + 1 && i + j < size - 1) ++j;
+    else if (i < j && i + j >= size - 1) ++i;
+    else if (i >= j && i + j > size - 1) --j;
+    else --i;
+    ++value;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+PrintArray(array);
